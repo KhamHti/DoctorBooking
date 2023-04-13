c0b69880f66717f62ec6caebbd38d8ccfff676cd
KhamHti <90329989+KhamHti@users.noreply.github.com>
2023-04-14 01:38:26 +0630

    update navigator

--- src/AppNavigator.js
+++ src/AppNavigator.js
@@ -1,17 +1,21 @@
 import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import Splash from './screens/Splash';
 import Home from './screens/Home';
+import BookAppointment from './screens/BookAppointment';
+import BookingSuccess from './screens/BookingSuccess';
 
 const Stack = createNativeStackNavigator();
 
 const AppNavigator = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator screenOptions={{ headerShown: false }}>
+      <Stack.Navigator screenOptions={{headerShown: false}}>
         <Stack.Screen name="Splash" component={Splash} />
         <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="BookAppointment" component={BookAppointment} />
+        <Stack.Screen name="BookingSuccess" component={BookingSuccess} />
       </Stack.Navigator>
     </NavigationContainer>
   );
