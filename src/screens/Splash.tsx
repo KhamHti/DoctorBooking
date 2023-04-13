e395ad561b7f73d44c6fba6a4011081976b9e3e8
KhamHti <90329989+KhamHti@users.noreply.github.com>
2023-04-13 13:22:11 +0630

    Splash
    create splash screen

--- src/screens/Splash.tsx
+++ src/screens/Splash.tsx
@@ -1,12 +1,27 @@
-import {View, Text} from 'react-native';
+import {View, Text, Image, StyleSheet} from 'react-native';
 import React from 'react';
 
 const Splash = () => {
   return (
-    <View>
-      <Text>Splash</Text>
+    <View style={styles.container}>
+      <Text>Doctor App</Text>
+      <Image source={require('../images/logo.png')} style={styles.logo} />
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#3C486B',
+  },
+  logo: {
+    width: 100,
+    height: 100,
+    tintColor: '#fff',
+  },
+});
 
 export default Splash;
