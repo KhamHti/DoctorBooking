49deaed3c09c2d8e2ea585b57f546f7a7e0fe60d
KhamHti <90329989+KhamHti@users.noreply.github.com>
2023-04-14 01:40:47 +0630

    update eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,10 @@
 module.exports = {
   root: true,
   extends: '@react-native-community',
-  plugins: ['react-hooks'],
+  plugins: ['react-hooks', 'react'],
   rules: {
-    'react-hooks/rules-of-hooks': 'error',
-    'react-hooks/exhaustive-deps': 'true', // <--- THIS IS THE NEW RULE to empty array dependencies
+    'react-hooks/rules-of-hooks': 2,
+    'react-hooks/exhaustive-deps': 0, // <--- THIS IS THE NEW RULE to empty array dependencies
+    'react-native/no-inline-styles': 0,
   },
 };
--- src/screens/Home.js
+++ src/screens/Home.js
@@ -7,55 +7,80 @@ import {
   Image,
   FlatList,
   ScrollView,
-} from "react-native";
-import React from "react";
-import HeaderComponent from "../components/HeaderComponent";
-import LinearGradient from "react-native-linear-gradient";
+} from 'react-native';
+import React from 'react';
+import HeaderComponent from '../components/HeaderComponent';
+import LinearGradient from 'react-native-linear-gradient';
+import BookButton from '../components/BookButton';
 
-const Home = () => {
+const Home = ({navigation}) => {
   return (
     <SafeAreaView style={styles.container}>
       <ScrollView showsVerticalScrollIndicator={false}>
         <View style={styles.container}>
           <HeaderComponent
-            title={"Doctor App"}
-            icon={require("../images/logo.png")}
+            title={'Doctor App'}
+            icon={require('../images/logo.png')}
           />
-          <Image source={require("../images/banner.jpg")} style={styles.banner} />
+          <Image
+            source={require('../images/banner.jpg')}
+            style={styles.banner}
+          />
           <Text style={styles.heading}>Select Categoary</Text>
-          <View style={{ marginLeft: 5 }}>
+          <View style={{marginLeft: 5}}>
             <FlatList
               data={[1, 1, 1, 1, 1, 1]}
               horizontal={true}
-              renderItem={({ item, index }) => (
+              renderItem={({item, index}) => (
                 <TouchableOpacity>
                   <LinearGradient
-                    colors={["#BFCCB5", "#7C96AB", "#3C486B"]}
-                    style={styles.linearGradient}
-                  >
-                    <Text style={styles.catName}>{"Category" + (index + 1)}</Text>
+                    colors={['#BFCCB5', '#7C96AB', '#3C486B']}
+                    style={styles.linearGradient}>
+                    <Text style={styles.catName}>
+                      {'Category' + (index + 1)}
+                    </Text>
                   </LinearGradient>
                 </TouchableOpacity>
               )}
             />
           </View>
           <Text style={styles.heading}>Top Rated Doctors</Text>
-          <View style={{ marginTop: 10, alignSelf: "center" }}>
+          <View style={{marginTop: 10, alignSelf: 'center'}}>
             <FlatList
               numColumns={2}
               data={[1, 1, 1, 1, 1, 1]}
-              renderItem={({ item, index }) => {
+              renderItem={({item, index}) => {
                 return (
                   <View style={styles.docItem}>
-                    <Image source={require('../images/doctor.png')} style={styles.docImg} />
-                    <Text style={styles.docName}>Doctor  {(index + 1)}</Text>
+                    <Image
+                      source={require('../images/doctor.png')}
+                      style={styles.docImg}
+                    />
+                    <Text style={styles.docName}>Doctor {index + 1}</Text>
                     <Text style={styles.docSpl}>Skin Specialist</Text>
-                    <Text style={[styles.status, {
-                      color: index / 3 === 0 ? "green" : "red",
-                      opacity: index / 3 === 0 ? 1 : 0.5
-                    }]}>{index / 3 == 0 ? "Avaibale" : "Busy"}</Text>
+                    <Text
+                      style={[
+                        styles.status,
+                        {
+                          color: index / 2 === 0 ? 'green' : 'red',
+                          opacity: index / 2 === 0 ? 1 : 0.3,
+                        },
+                      ]}>
+                      {index / 2 === 0 ? 'Avaibale' : 'Busy'}
+                    </Text>
+                    <BookButton
+                      width={150}
+                      height={35}
+                      status={index / 2 === 0 ? true : false}
+                      title={'Book Appointment'}
+                      onClick={() => {
+                        if (index / 2 === 0) {
+                          navigation.navigate('BookAppointment');
+                        }
+                      }}
+                    />
                   </View>
-                )
+                );
               }}
             />
           </View>
@@ -68,24 +93,24 @@ const Home = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff"
+    backgroundColor: '#fff',
   },
   backButton: {
     padding: 50,
     marginHorizontal: 50,
-    backgroundColor: "#3C486B",
+    backgroundColor: '#3C486B',
   },
   banner: {
-    width: "100%",
+    width: '100%',
     height: 250,
-    alignSelf: "center",
+    alignSelf: 'center',
     marginTop: 10,
     borderRadius: 10,
   },
   heading: {
-    color: "#000",
+    color: '#000',
     fontSize: 18,
-    fontWeight: "800",
+    fontWeight: '800',
     marginTop: 10,
     marginLeft: 25,
   },
@@ -95,54 +120,54 @@ const styles = StyleSheet.create({
     borderRadius: 10,
     marginLeft: 10,
     marginVertical: 10,
-    alignItems: "center",
-    justifyContent: "center",
+    alignItems: 'center',
+    justifyContent: 'center',
   },
   catName: {
     fontSize: 18,
-    fontWeight: "600",
-    color: "#fff",
+    fontWeight: '600',
+    color: '#fff',
   },
   docItem: {
-    width: "45%",
-    height: 200,
-    backgroundColor: "#fff",
+    width: '45%',
+    // height: 200,
+    backgroundColor: '#fff',
     // backgroundColor: "#3C486B",
     borderRadius: 15,
     borderWidth: 1,
-    margin: 10
+    margin: 10,
   },
   docImg: {
     width: 69,
     height: 69,
-    alignSelf: "center",
-    marginTop: 5
+    alignSelf: 'center',
+    marginTop: 5,
   },
   docName: {
     fontSize: 18,
-    color: "#3C486B",
-    fontWeight: "700",
-    alignSelf: "center",
-    marginTop: 5
+    color: '#3C486B',
+    fontWeight: '700',
+    alignSelf: 'center',
+    marginTop: 5,
   },
   docSpl: {
     fontSize: 13,
-    color: "#3C486B",
-    fontWeight: "800",
-    alignSelf: "center",
+    color: '#3C486B',
+    fontWeight: '800',
+    alignSelf: 'center',
     marginTop: 5,
-    borderColor: "#3C486B",
+    borderColor: '#3C486B',
     borderWidth: 1,
     padding: 5,
     borderRadius: 10,
   },
   status: {
     fontSize: 13,
-    color: "#3C486B",
-    fontWeight: "800",
-    alignSelf: "center",
-    marginTop: 10,
-  }
+    color: '#3C486B',
+    fontWeight: '800',
+    alignSelf: 'center',
+    marginTop: 5,
+  },
 });
 
 export default Home;
